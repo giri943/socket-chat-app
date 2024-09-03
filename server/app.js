7960179e490861ad389d23ce2e9c223e32fe1137
Girish <girish.soman@schbang.com>
2024-09-03 17:34:48 +0530

    feat:Initial Backend server created

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 3000
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("Hellow world")
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
