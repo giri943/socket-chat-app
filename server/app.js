97b6cee4fb59f2cbce66ac7687784671416147e1
Girish <girish.soman@schbang.com>
2024-09-10 16:51:46 +0530

    fix:created baisc server instance using express and web socket

--- server/app.js
+++ server/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const createServer = require("http").createServer
+const { Server } = require("socket.io");
+const cors = require('cors');
 const port = 3000
 const app = express();
-
-app.get("/", (req, res)=> {
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+app.get("/", (req, res) => {
     res.send("Hellow world")
 })
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("user connected")
+    console.log("Id", socket.id)
+})
+
+server.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
